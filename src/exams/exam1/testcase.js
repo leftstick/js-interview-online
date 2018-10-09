c77ca9d0793b3d2380b563cd61dfb0347b3b8b60
Howard.Zuo <leftstick@qq.com>
2018-10-09 14:04:28 +0800

    fix: add case for isString

--- src/exams/exam1/testcase.js
+++ src/exams/exam1/testcase.js
@@ -3,5 +3,6 @@ export default [
   `assert.equal(isString(12445), false, '原始数值类型校验失败')`,
   `assert.equal(isString(undefined), false, '未初始化变量校验失败')`,
   `assert.equal(isString(null), false, '空值校验失败')`,
-  `assert(isString(new String('hello')), '字符串对象校验失败')`
+  `assert(isString(new String('hello')), '字符串对象校验失败')`,
+  `assert.equal(isString({ name: 'aaa' }), false, '字面量类型校验失败')`
 ]
