200646dcaa4ccbb021721966bd6a591fe3873177
Howard.Zuo <leftstick@qq.com>
2020-04-01 10:46:49 +0800

    feat: new exam

--- src/exams/exam9/testcase.js
+++ src/exams/exam9/testcase.js
@@ -1,59 +1,19 @@
 export default [
-  `const app = new DI()
-
-app.register('duck', {
-  fly() {
-    return 'hello flying'
-  }
-});
-
-app.run(['duck', function(duck) {
-  assert.equal(duck.fly(), 'hello flying', '字面量注入测试失败')
-  done()
-}])`,
-
-  `const app = new DI();
-
-app.register('woman', function() {
-    this.cry = function() {
-        return 'crying wawawa!'
+  `
+  const treeNode = {
+    value: 10,
+    left: {
+      value: 8,
+      left: {
+        value: 15
+      },
+      right: {
+        value: 7
+      }
+    },
+    right: {
+      value: 9
     }
-})
-
-app.run(['woman', function(woman) {
-    assert.equal(woman.cry(), 'crying wawawa!', '类注入测试失败')
-    done()
-}])`,
-
-  `const app = new DI();
-
-app.register('people', function() {
-  this.yell = function() {
-      return 'don\\'t go'
   }
-})
-
-app.register('puppy', function() {
-  this.bark = function() {
-      return 'wow'
-  }
-})
-
-app.run(['puppy', 'people', function(puppy, people) {
-  assert.equal(puppy.bark(), 'wow', '多实例puppy注入测试失败')
-  assert.equal(people.yell(), 'don\\'t go', '多实例people注入测试失败')
-  done()
-}])`,
-
-  `const app = new DI();
-
-app.register('puppy', function() {
-  this.bark = function() {
-    return 'wow'
-  }
-})
-
-assert.throws(function() {
-  app.run(['man', function(man) {}])
-}, Error, '不存在实例测试失败')`
+  assert.equal(maxDepth(treeNode), 3, '基本测试失败')`
 ]
--- src/helpers/exam.js
+++ src/helpers/exam.js
@@ -6,7 +6,7 @@ import { removeComments } from '@/helpers/object'
 export const routes = [
   {
     path: '/exam1',
-    title: '如何判断一个变量是否字符串',
+    title: '01. 如何判断一个变量是否字符串',
     content: getCodeContent(() => import(/* webpackChunkName: "exam1" */ '../exams/exam1/question.txt')),
     contentValidator(code) {
       return /function\s*isString\(value\)\s*{[\s\S]*}/.test(code)
@@ -19,7 +19,7 @@ export const routes = [
   },
   {
     path: '/exam2',
-    title: '完成一个简单的使柯里化(currying)函数',
+    title: '02. 完成一个简单的使柯里化(currying)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam2" */ '../exams/exam2/question.txt')),
     contentValidator(code) {
       return /function\s*currying\(func\)\s*{[\s\S]*}/.test(code)
@@ -32,7 +32,7 @@ export const routes = [
   },
   {
     path: '/exam3',
-    title: '完成一个duplicate(重复)函数',
+    title: '03. 完成一个duplicate(重复)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam3" */ '../exams/exam3/question.txt')),
     contentValidator(code) {
       return /function\s*duplicate\(array\)\s*{[\s\S]*}/.test(code)
@@ -45,7 +45,7 @@ export const routes = [
   },
   {
     path: '/exam4',
-    title: '完成一个map(变型)函数',
+    title: '04. 完成一个map(变型)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam4" */ '../exams/exam4/question.txt')),
     contentValidator(code) {
       return /function\s*map\(arr,\s*func,\s*ctx\)\s*{[\s\S]*}/.test(code)
@@ -58,7 +58,7 @@ export const routes = [
   },
   {
     path: '/exam5',
-    title: '完成一个value(取值)函数',
+    title: '05. 完成一个value(取值)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam5" */ '../exams/exam5/question.txt')),
     contentValidator(code) {
       return /function\s*value\(anything\)\s*{[\s\S]*}/.test(code)
@@ -71,7 +71,7 @@ export const routes = [
   },
   {
     path: '/exam6',
-    title: '完成一个camelcase(驼峰化)函数',
+    title: '06. 完成一个camelcase(驼峰化)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam6" */ '../exams/exam6/question.txt')),
     contentValidator(code) {
       return /function\s*camelcase\(str\)\s*{[\s\S]*}/.test(code)
@@ -84,7 +84,7 @@ export const routes = [
   },
   {
     path: '/exam7',
-    title: '完成一个thunkify(形式转换)函数',
+    title: '07. 完成一个thunkify(形式转换)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam7" */ '../exams/exam7/question.txt')),
     contentValidator(code) {
       return /function\s*thunkify\(func\)\s*{[\s\S]*}/.test(code)
@@ -97,7 +97,7 @@ export const routes = [
   },
   {
     path: '/exam8',
-    title: '完成一个times(多次生成)函数',
+    title: '08. 完成一个times(多次生成)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam8" */ '../exams/exam8/question.txt')),
     contentValidator(code) {
       return /function\s*times\(n,\s*func\)\s*{[\s\S]*}/.test(code)
@@ -110,26 +110,20 @@ export const routes = [
   },
   {
     path: '/exam9',
-    title: '完成一个DI(依赖注入)类',
+    title: '09. 完成一个二叉树深度查询函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam9" */ '../exams/exam9/question.txt')),
-    inputFuncName: 'DI',
     contentValidator(code) {
-      return /class\s*DI\s*{[\s\S]*register\(name,\s*instance\)\s*{[\s\S]*}[\s\S]*run\(arr\)\s*{[\s\S]*}[\s\S]*}/.test(
-        code
-      )
+      return /function\s*maxDepth\(treeNode\)\s*{[\s\S]*}/.test(code)
     },
     contentIntegrityValidator(code) {
       const realCode = removeComments(code)
-      // eslint-disable-next-line
-      return /^class\s*DI\s*{[\s\S]*register\(name,\s*instance\)\s*{[\s\S]*}[\s\S]*run\(arr\)\s*{[\s\S]*}[\s\S]*}$/.test(
-        realCode
-      )
+      return /^function\s*maxDepth\(treeNode\)\s*{[\s\S]*}$/.test(realCode)
     },
     testCase: getTestCaseRunner(() => import(/* webpackChunkName: "case9" */ '../exams/exam9/testcase'))
   },
   {
     path: '/exam10',
-    title: '完成一个difference(找不同)函数',
+    title: '10. 完成一个difference(找不同)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam10" */ '../exams/exam10/question.txt')),
     contentValidator(code) {
       return /function\s*difference\(src,\s*target\)\s*{[\s\S]*}/.test(code)
@@ -142,7 +136,7 @@ export const routes = [
   },
   {
     path: '/exam11',
-    title: '完成一个flatten(扁平化)函数',
+    title: '11. 完成一个flatten(扁平化)函数',
     content: getCodeContent(() => import(/* webpackChunkName: "exam11" */ '../exams/exam11/question.txt')),
     contentValidator(code) {
       return /function\s*flatten\(arr\)\s*{[\s\S]*}/.test(code)
